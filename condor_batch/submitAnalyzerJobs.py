6ed24ac56ad04cd8db013520bf7319bf0b5d4337
GraceCummings <cummingsge@vcu.edu>
2024-05-16 10:52:11 -0500

    submission to condor works, but executable fails

--- condor_batch/submitAnalyzerJobs.py
+++ condor_batch/submitAnalyzerJobs.py
@@ -12,8 +12,9 @@ if __name__=='__main__':
     args = parser.parse_args()
 
     #get a name
-    sampleName = args.sampleTextFile.split("_13TeV")[0]
-    outFileName = sampleName+"_llpanalyzer.root
+    protoName = args.sampleTextFile.split("_13TeV")[0]
+    sampleName = protoName.split("/")[-1]
+    outFileName = sampleName+"_llpanalyzer.root"
     
     #Check there is a text file to run over
     if (args.sampleTextFile is None):
@@ -31,7 +32,7 @@ if __name__=='__main__':
     print("Creating tarball of working area")
     tarballName = "cmsswTar.tar.gz"
     if not os.path.exists(tarballName):
-        os.system("tar -hcf "+tarballName+" ../../../CMSSW_10_6_30")
+        os.system("tar -hcf "+tarballName+" ../../../../CMSSW_10_6_30")
     else:
         print('FOUND A TARBALL -- USING, BE CAREFULL!!!!!')
 
@@ -72,9 +73,11 @@ if __name__=='__main__':
         jdl.write("Output = condorMonitoringOutput/{0}/{1}_out.stdout\n".format(str(date.today()),sampleName))
         jdl.write("Error = condorMonitoringOutput/{0}/{1}_err.stder\n".format(str(date.today()),sampleName))
         jdl.write("Log = condorMonitoringOutput/{0}/{1}_log.log\n".format(str(date.today()),sampleName))
-        jdl.write("Executable = selections.sh\n")
+        jdl.write("Executable = runAnalyzer.sh\n")
         jdl.write(argu)
         jdl.write('+DESIRED_Sites="T3_US_Baylor,T2_US_Caltech,T3_US_Colorado,T3_US_Cornell,T3_US_FIT,T3_US_FNALLPC,T3_US_Omaha,T3_US_JHU,T3_US_Kansas,T2_US_MIT,T3_US_NotreDame,T2_US_Nebraska,T3_US_NU,T3_US_OSU,T3_US_Princeton_ICSE,T2_US_Purdue,T3_US_Rice,T3_US_Rutgers,T3_US_MIT,T3_US_NERSC,T3_US_SDSC,T3_US_FIU,T3_US_FSU,T3_US_OSG,T3_US_TAMU,T3_US_TTU,T3_US_UCD,T3_US_UCSB,T2_US_UCSD,T3_US_UMD,T3_US_UMiss,T2_US_Vanderbilt,T2_US_Wisconsin"')
+        jdl.write("\n")
+        jdl.write('+ApptainerImage = "/cvmfs/singularity.opensciencegrid.org/cmssw/cms:rhel7"')
         jdl.write("\n")
         jdl.write("Queue 1\n")#Not sure about this one
         jdl.close()
